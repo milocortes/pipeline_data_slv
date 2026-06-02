b01bc37815e85986d7921de3a1f2be18e69f5554
Hermilo Cortes <hermilocg@hotmail.com>
2026-06-02 15:51:20 -0500

    agrega funcion que obtiene ip del dns del servicio para construir la ruta de acceso

--- services/streamlit_app/environment.sh
+++ services/streamlit_app/environment.sh
@@ -7,7 +7,8 @@ export service_account_gee="geoespacial@pib-geoespacial.iam.gserviceaccount.com"
 export gee_project="pib-geoespacial"
 
 # Storage environment variables
-export AWS_ENDPOINT_URL='http://127.0.0.1:9000'  # RustFS server URL
+export RUSTFS_DNS="localhost" # RustFS server URL
+export RUSTFS_PORT="9000"
 export AWS_REGION="us-east-1"
 export AWS_ACCESS_KEY_ID="rustfsadmin"
 export AWS_SECRET_ACCESS_KEY="rustfsadmin"
