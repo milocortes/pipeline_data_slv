643496dc44c2fb8a872b86fddf4aa764ea36ae9b
Hermilo Cortes <hermilocg@hotmail.com>
2026-07-05 16:21:57 -0500

    documenta deploy de docker compose

--- deploy_docker_compose_dev.sh
+++ deploy_docker_compose_dev.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+# Carga las variables de ambiente definidas en el archivo .env-dev
 source .env-dev
+
+# Añadimos permisos adicionales al archivo /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
+
+# Sobreescribe el archivo Docker compose base de Apache Airflow para agregar las rutas específicas de los microservicios.
 docker compose --env-file  .env-dev -f ./official-airflow-docker-compose.yml -f ./services-docker-compose-dev.yml config > docker-compose-dev.yml
+
+# Ejecuta archivo de Docker compose.
 docker compose -f docker-compose-dev.yml up  -d --build --force-recreate
 
